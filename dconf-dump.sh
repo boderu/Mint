cef7b2dbbfc30783ad3f64452ea7b7598dd00fdb
Rüdiger Bode <r.git@bode.onl>
2024-04-22 23:31:10 +0200

    NerdFonts

--- dconf-dump.sh
+++ dconf-dump.sh
@@ -6,4 +6,5 @@ dconf dump /org/x/pix/ > org.x.pix.dconf
 dconf dump /org/x/reader/ > org.x.reader.dconf
 dconf dump /org/gnome/calculator/ > org.gnome.calculator.dconf
 dconf dump /org/gnome/terminal/ > org.gnome.terminal.dconf
+dconf dump /org/gnome/desktop/interface/ > org.gnome.desktop.interface.dconf
 dconf dump /apps/guake/ > apps.guake.dconf
