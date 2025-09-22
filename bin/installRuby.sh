9c41dc9f7b678a87393e474a5287e5a3b94a608d
Rüdiger Bode <r.git@bode.onl>
2025-09-22 11:15:58 +0200

    Ruby Gems erweitert

--- bin/installRuby.sh
+++ bin/installRuby.sh
@@ -5,5 +5,6 @@ echo ; echo "Additional Ruby packages"
 echo ; sudo gem install ruby-lsp
 echo ; sudo gem install asciidoctor-diagram
 echo ; sudo gem install roo
+echo ; sudo gem install ceedling
 
 # EOF
